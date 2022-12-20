0413851e5ccce6856280de36a6e72a8c2790bd7e
lamia1990 <kessoumlamia@gmail.com>
2022-12-19 21:36:01 -0500

    Java Exam

--- src/learning_to_read_and_write/TextFileReadAndWrite.java
+++
@@ -1,8 +0,0 @@
-package learning_to_read_and_write;
-
-public class TextFileReadAndWrite {
-    public static void main(String[] args) {
-        System.out.println("Hello everyone, how are you today? \nThe weather in NC is a bit chilli today");
-    }
-
-}
